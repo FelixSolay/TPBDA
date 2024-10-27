e2d5004e59728505c71720b70ba80a23418cc7d1
Weidmann German A. <82054626+WeidmannGermanA@users.noreply.github.com>
2024-10-27 20:23:39 -0300

    Cambios en creation

--- Script Creation.sql
+++ Script Creation.sql
@@ -18,6 +18,33 @@ go
 create table ddbba.sucursal(
 	Id int primary key,
 	Nombre char(20),
-	Localidad char(20),
+	Ciudad char(20),
+)
+go
+
+create table ddbba.catalogo(
+	Id int primary key,
+	cant_prod int,
+
+)
+go
+
+create table ddbba.cliente(
+	IDVneta int primary key,
+	Cliente cliente foreign key
+	
+)
+go
+
+create table ddbba.detalleVenta(
+	IDVneta int primary key,
+	
+)
+go
+
+create table ddbba.Venta(
+	IDVneta int primary key,
+	Cliente cliente foreign key
+	
 )
 go
