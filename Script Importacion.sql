93053cea4746ac444c85e4c5fca18c289b6a121f
Snarclains <49152993+Snarclains@users.noreply.github.com>
2024-11-04 22:22:47 -0300

    Separacion de Scripts, Adaptacion de BULK a memoria

--- Script Importacion.sql
+++ Script Importacion.sql
@@ -1,33 +1,40 @@
 use AuroraVentas
 go
 
-CREATE OR ALTER PROCEDURE ddbba.BulkCategoria
+CREATE OR ALTER PROCEDURE ddbba.BulkProducto
 	@Path NVARCHAR(max)
 AS
 BEGIN
+	SET TRANSACTION ISOLATION LEVEL READ COMMITTED
+
+	BEGIN TRANSACTION 
 	DECLARE @SQLBulk NVARCHAR(max)
 
-	SET @SQLBulk = 'BULK INSERT ddbba.producto 
-					FROM '''+ @Path +''' 
+	SET @SQLBulk = 'BULK INSERT ddbba.CSVMem
+					FROM ''' + @Path + ''' 
 					WITH
 					(
 						FIRSTROW = 2,
 						DATAFILETYPE = ''char'',
 						FIELDTERMINATOR = '','',
-						ROWTERMINATOR = ''\n'',
-						TABLOCK
+						ROWTERMINATOR = ''\n''
 					)'
 
 	BEGIN TRY
 		EXEC sp_executesql @SQLBulk
+
+		INSERT ddbba.producto(Nombre, Precio, PrecioReferencia, UnidadReferencia, Fecha)
+			SELECT Nombre, Precio, PrecioReferencia, UnidadReferencia, Fecha
+				FROM ddbba.CSVMem WITH (SNAPSHOT)
+
+				commit transaction
 	END TRY
 	BEGIN CATCH
 		SELECT ERROR_MESSAGE()
+		ROLLBACK TRANSACTION
 	END CATCH
 END
+GO
 
-EXEC ddbba.BulkCategoria @Path = 'C:\Users\juanp\Downloads\catalogo2.csv'
-
-select *
-	from ddbba.producto
-
+EXEC ddbba.BulkProducto @Path = 'C:\Users\juanp\Downloads\catalogo2.csv'
+go
