2ba6e1818c6a0fa799e4085e4798669edd3d314e
Alan Aguirre <121577560+alagui@users.noreply.github.com>
2024-11-06 21:04:24 -0300

    Notas para cambiar cuando tengamos las tablas

--- Script SP NC y ND.sql
+++ Script SP NC y ND.sql
@@ -6,6 +6,7 @@ CREATE OR ALTER PROCEDURE ddbba.GenerarNotaCredito
     @VentaID INT,
     @Motivo NVARCHAR(255),
     @MontoCredito DECIMAL(9, 2)
+	-- aca deberia recibir las opciones de otro producto o el dinero
 AS
 BEGIN
     BEGIN TRANSACTION;
@@ -14,6 +15,13 @@ BEGIN
         DECLARE @TotalVenta DECIMAL(9, 2);
         SELECT @TotalVenta = Total FROM ddbba.venta WHERE IDVenta = @VentaID;
         
+
+		--agregar verificacion de rol del empleado
+
+
+
+		-- añadir opcion de pedir otro producto de similar valor 
+
         IF @MontoCredito > @TotalVenta
         BEGIN
             PRINT 'El monto de la nota de credito no puede exceder el total de la venta';
@@ -24,6 +32,12 @@ BEGIN
        
        -- EXEC ddbba.InsertarLog 'NotaCredito', @Motivo; ya para anticipar lo del log ver como lo manejamos dsp
 
+	   -- para saber si esta pago solamente deberia checkear si existe el pago, ya que el valor pago del comprobante admite NULL
+
+
+
+
+	   -- hacerlo aca
         
         UPDATE ddbba.venta
         SET Total = Total - @MontoCredito
