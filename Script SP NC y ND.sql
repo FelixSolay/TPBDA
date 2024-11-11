bf0ac79e42d4c88bdc32b37090fa5344f874931e
Alan Aguirre <121577560+alagui@users.noreply.github.com>
2024-11-11 16:32:37 -0300

    Agrego comentadas opciones de encriptación

--- Script SP NC y ND.sql
+++ Script SP NC y ND.sql
@@ -50,7 +50,7 @@ BEGIN
         FROM facturacion.comprobante
         WHERE tipo = 'NC';
 
-        -- Insertar el nuevo comprobante de tipo 'NC' utilizando el SP InsertarComprobante
+        
         DECLARE @Fecha DATETIME = GETDATE();
         EXEC facturacion.InsertarComprobante    --- no se si falta agregar lo de la hora en este SP  -- seria asi: CONVERT(TIME, GETDATE())
             @tipo = 'NC',
@@ -64,7 +64,7 @@ BEGIN
 
         DECLARE @NotaCreditoID INT = SCOPE_IDENTITY();
 
-		 -- Registrar el detalle de la nota de crédito en forma de producto específico de la misma categoría
+		
         IF @DevolucionProducto = 1
         BEGIN
             DECLARE @PrecioProducto DECIMAL(9, 2), @IdCategoriaOriginal INT, @IdCategoriaProducto INT;
@@ -233,35 +233,176 @@ REVERT;
 GO
 
 /*
+--------------------------- encriptacion de datos empleado -----------------------------------------
 USE COM2900G09
 GO
 -- Agregamos campo para los datos cifrados
-ALTER TABLE infraestructura.Empleado
-	ADD DireccionCifrada VARBINARY(256);
+ALTER TABLE infraestructura.empleado
+    ADD DireccionCifrada VARBINARY(MAX),
+        EmailPersonalCifrado VARBINARY(MAX);
 GO
 
 -- Obtenemos la clave de cifrado 
-DECLARE @FraseClaveCargada NVARCHAR(128);
-SET @FraseClaveCargada  = 'CifraDatos';
+DECLARE @FraseClave NVARCHAR(128) = 'CifraDatos';
 
 -- Ciframos los datos personales de los empleados.
 -- Agrega un Hash (PK IdEmpleado al cifrado)
 
-UPDATE infraestructura.Empleado
-SET DireccionCifrada = EncryptByPassPhrase(@FraseClaveCargada, Direccion, 1 ,CONVERT(varbinary, IdEmpleado))
-WHERE IdEmpleado = '1';
 
+
+------------------------- ACTUALIZACION DE TABLA EMPLEADOS DESPUES DE HABER SIDO IMPORTADOS -----------------------------
+
+UPDATE infraestructura.empleado
+SET 
+    DireccionCifrada = EncryptByPassPhrase(@FraseClave, Direccion, 1, CONVERT(VARBINARY, Legajo)),
+    EmailPersonalCifrado = EncryptByPassPhrase(@FraseClave, EmailPersonal, 1, CONVERT(VARBINARY, Legajo));
 GO
 
--- PARA DESENCRIPTAR 
+-- vemos que se encripto correctamente 
 
-DECLARE @FraseClaveCargadaPorUsuario NVARCHAR(128);
-SET @FraseClaveCargadaPorUsuario  = 'CifraDatos'
+SELECT Legajo, DireccionCifrada, EmailPersonalCifrado
+FROM infraestructura.empleado;
+
+-- PARA DESENCRIPTAR ------
+
+DECLARE @FraseClave NVARCHAR(128) = 'CifraDatos';
 
 SELECT 
-    IdEmpleado,
-    CONVERT(NVARCHAR(MAX), DecryptByPassPhrase(@FraseClave, DireccionCifrada, 1, CONVERT(VARBINARY, IdEmpleado))) AS DireccionDesencriptada
-FROM infraestructura.Empleado;
+    Legajo,
+    CONVERT(NVARCHAR(MAX), DecryptByPassPhrase(@FraseClave, DireccionCifrada, 1, CONVERT(VARBINARY, Legajo))) AS DireccionDesencriptada,
+    CONVERT(NVARCHAR(MAX), DecryptByPassPhrase(@FraseClave, EmailPersonalCifrado, 1, CONVERT(VARBINARY, Legajo))) AS EmailPersonalDesencriptado
+FROM infraestructura.empleado;
+
+-----------------------------------------------------------------------------
+
+
+
+
+----------- SI QUISIERA HACERLO AL MOMENTO DE IMPORTARLO --------------------
+CREATE OR ALTER PROCEDURE infraestructura.ImportEmpleados
+	@Path NVARCHAR(MAX)
+AS
+BEGIN
+	create table #Empleados(
+		Legajo		  VARCHAR(MAX),
+		Nombre		  VARCHAR(MAX),
+		Apellido	  VARCHAR(MAX),
+		DNI			  VARCHAR(MAX),
+		Direccion	  VARCHAR(MAX),
+		EmailPersonal VARCHAR(MAX),
+		EmailEmpresa  VARCHAR(MAX),
+		CUIL		  VARCHAR(MAX),
+		Turno		  VARCHAR(MAX),
+		Cargo		  VARCHAR(MAX),
+		Sucursal	  VARCHAR(MAX),
+	)
+	SET TRANSACTION ISOLATION LEVEL READ COMMITTED
+	BEGIN TRANSACTION 
+	DECLARE @SQL   NVARCHAR(MAX)
+	DECLARE @OLEDB NVARCHAR(MAX) = 'Microsoft.ACE.OLEDB.16.0'
+	DECLARE @EXCEL NVARCHAR(MAX) = 'Excel 12.0'
+
+	-------------------CAMBIO-------------------------
+	 DECLARE @FraseClave NVARCHAR(128) = 'CifraDatos'; 
+	 -------------------------------------------------
+
+
+	BEGIN TRY
+		-- Informacion_complementaria.xlsx -> Empleados
+		SET @SQL = 'INSERT INTO #Empleados(Legajo, Nombre, Apellido, DNI, Direccion, EmailPersonal, EmailEmpresa, CUIL, Cargo, Sucursal, Turno)
+						SELECT *
+							FROM OPENROWSET(''' + @OLEDB + ''',
+											''' + @EXCEL + ';HDR=YES;Database=' + @Path + '\Informacion_complementaria.xlsx'',
+											''SELECT * FROM [Empleados$]'',
+											FIRSTROW = 2)'
+
+		EXEC sp_executesql @SQL
+
+		INSERT INTO infraestructura.empleado(Legajo, Nombre, Apellido, DNI, Direccion, EmailPersonal, EmailEmpresa, CUIL, Cargo, Sucursal, Turno)
+		----CAMBIO-----------------------
+		SELECT 
+            Legajo,
+            Nombre,
+            Apellido,
+            DNI,
+            -- Cifrar Direccion y EmailPersonal al insertarlos en la tabla destino
+            EncryptByPassPhrase(@FraseClave, Direccion, 1, CONVERT(VARBINARY, Legajo)) AS DireccionCifrada,
+            EncryptByPassPhrase(@FraseClave, EmailPersonal, 1, CONVERT(VARBINARY, Legajo)) AS EmailPersonalCifrada,
+            EmailEmpresa,
+            CUIL,
+            Cargo,
+            Sucursal,
+            Turno
+        FROM #Empleados;
+		--------------------------------
+			
+			--SELECT *                    --- esto se iria reemplazado por lo de arriba
+			--	FROM #Empleados				-- idem
+
+		DROP TABLE #Empleados
+
+		COMMIT TRANSACTION 
+	END TRY
+	BEGIN CATCH
+		SELECT ERROR_MESSAGE()
+		ROLLBACK TRANSACTION
+	END CATCH
+END
 GO
+
+--------------------------- para cualquiera de los casos para los siguietntes ingresos se modificaria el insert --------
+
+CREATE OR ALTER PROCEDURE infraestructura.InsertarEmpleado
+    @Legajo INT,
+    @Nombre VARCHAR(30),
+    @Apellido VARCHAR(30),
+    @DNI INT,
+    @Direccion VARCHAR(100),
+    @EmailPersonal VARCHAR(100),
+    @EmailEmpresa VARCHAR(100),
+    @CUIL CHAR(11),
+    @Turno CHAR(16),
+    @Cargo INT,
+    @Sucursal INT
+AS
+BEGIN
+    BEGIN TRANSACTION;
+    BEGIN TRY
+		
+        --INSERT INTO infraestructura.Empleado (Legajo, Nombre, Apellido, DNI, Direccion, EmailPersonal, EmailEmpresa, CUIL, Turno, Cargo, Sucursal)
+		--VALUES (@Legajo, @Nombre, @Apellido, @DNI, @Direccion, @EmailPersonal, @EmailEmpresa, @CUIL, @Turno, @Cargo, @Sucursal);
+
+		-----------------CAMBIO--------------------------
+		INSERT INTO infraestructura.empleado (Legajo, Nombre, Apellido, DNI, DireccionCifrada, EmailPersonalCifrado, EmailEmpresa, CUIL, Turno, Cargo, Sucursal)
+		VALUES 
+            (
+                @Legajo,
+                @Nombre,
+                @Apellido,
+                @DNI,
+                -- Encriptación de Direccion y EmailPersonal al momento de la inserción
+                EncryptByPassPhrase(@FraseClave, @Direccion, 1, CONVERT(VARBINARY, @Legajo)),
+                EncryptByPassPhrase(@FraseClave, @EmailPersonal, 1, CONVERT(VARBINARY, @Legajo)),
+                @EmailEmpresa,
+                @CUIL,
+                @Turno,
+                @Cargo,
+                @Sucursal
+            );
+        ----------------------------------------------------------
+
+
+        COMMIT TRANSACTION;
+        PRINT 'Empleado insertado correctamente.';
+    END TRY
+    BEGIN CATCH
+        ROLLBACK TRANSACTION;
+        PRINT 'Error al intentar insertar el Empleado: ' + ERROR_MESSAGE();
+    END CATCH;
+END;
+GO
+
+
+
 
 */
