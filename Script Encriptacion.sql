a00c5438ebdd8e879e1e26eb4b2207b96f26e04e
Alan Aguirre <121577560+alagui@users.noreply.github.com>
2024-11-25 17:32:06 -0300

    Correcciones finales, funcional.

--- Script Encriptacion.sql
+++ Script Encriptacion.sql
@@ -1,102 +1,112 @@
-/*
 --------------------------- encriptacion de datos empleado -----------------------------------------
 USE COM2900G09
 GO
--- Agregamos campo para los datos cifrados
-ALTER TABLE infraestructura.empleado
-    ADD DireccionCifrada VARBINARY(256),
-        EmailPersonalCifrado VARBINARY(256);
-GO
 
----- dropeamos la direccion sin cifrar? ------
-
-
--- Obtenemos la clave de cifrado 
-DECLARE @FraseClave NVARCHAR(128) = 'CifraDatos';
-
--- Ciframos los datos personales de los empleados.
-
-
-
-------------------------- ACTUALIZACION DE TABLA EMPLEADOS DESPUES DE HABER SIDO IMPORTADOS -----------------------------
-
-UPDATE infraestructura.empleado
-SET 
-    DireccionCifrada = EncryptByPassPhrase(@FraseClave, Direccion, 1, CONVERT(VARBINARY, Legajo)),
-    EmailPersonalCifrado = EncryptByPassPhrase(@FraseClave, EmailPersonal, 1, CONVERT(VARBINARY, Legajo));
-GO
-
--- vemos que se encripto correctamente 
-
-SELECT Legajo, DireccionCifrada, EmailPersonalCifrado
-FROM infraestructura.empleado;
-
--- PARA DESENCRIPTAR ------
-
-DECLARE @FraseClave NVARCHAR(128) = 'CifraDatos';
+-- Vemos los datos no encriptados primero
 
 SELECT 
     Legajo,
-    CONVERT(NVARCHAR(MAX), DecryptByPassPhrase(@FraseClave, DireccionCifrada, 1, CONVERT(VARBINARY, Legajo))) AS DireccionDesencriptada,
-    CONVERT(NVARCHAR(MAX), DecryptByPassPhrase(@FraseClave, EmailPersonalCifrado, 1, CONVERT(VARBINARY, Legajo))) AS EmailPersonalDesencriptado
-FROM infraestructura.empleado;
+    DNI,
+    Direccion,
+    CUIL
+FROM infraestructura.empleado
+GO
 
------------------------------------------------------------------------------
-
-
---------------------------- para cualquiera de los casos para los siguietntes ingresos se modificaria el insert --------
-
-CREATE OR ALTER PROCEDURE infraestructura.InsertarEmpleado
-    @Legajo INT,
-    @Nombre VARCHAR(30),
-    @Apellido VARCHAR(30),
-    @DNI INT,
-    @Direccion VARCHAR(100),
-    @EmailPersonal VARCHAR(100),
-    @EmailEmpresa VARCHAR(100),
-    @CUIL CHAR(11),
-    @Turno CHAR(16),
-    @Cargo INT,
-    @Sucursal INT
+--------------------------- PREPARAMOS LAS TABLAS ----------------------------------------------------
+CREATE OR ALTER PROCEDURE PrerpararEncriptacionEmpleados
 AS
 BEGIN
+    IF NOT EXISTS (
+        SELECT 1 
+        FROM sys.symmetric_keys 
+        WHERE name = 'clave_simetrica_emp'
+    )
+    BEGIN
+        CREATE SYMMETRIC KEY clave_simetrica_emp WITH ALGORITHM = AES_256
+        ENCRYPTION BY PASSWORD = 'clavesegura1';
+    END;
+
+    DROP INDEX IF EXISTS idx_empleado_dni ON infraestructura.empleado;
+
+
+	ALTER TABLE infraestructura.empleado DROP CONSTRAINT UQ_Empleado_DNI
+	ALTER TABLE infraestructura.empleado DROP CONSTRAINT UQ_Empleado_CUIL
+	ALTER TABLE infraestructura.empleado DROP CONSTRAINT CHK_Empleado_CUIL
+
+	ALTER TABLE infraestructura.empleado ALTER COLUMN DNI nvarchar(256)
+	ALTER TABLE infraestructura.empleado ALTER COLUMN Direccion nvarchar(256)
+	ALTER TABLE infraestructura.empleado ALTER COLUMN CUIL nvarchar(256)
+	ALTER TABLE infraestructura.empleado ADD Encriptado bit default 0 NOT NULL
+
+	ALTER TABLE infraestructura.empleado ADD CONSTRAINT UQ_Empleado_DNI UNIQUE (DNI);
+	ALTER TABLE infraestructura.empleado ADD CONSTRAINT UQ_Empleado_CUIL UNIQUE (CUIL);
+
+	CREATE NONCLUSTERED INDEX idx_empleado_dni
+    ON infraestructura.empleado (DNI);
+
+
+END;
+GO
+
+EXEC PrerpararEncriptacionEmpleados;
+GO
+
+------------------------- SP DE ENCRIPTACION -----------------------------
+
+CREATE OR ALTER PROCEDURE EncriptarEmpleados
+AS
+BEGIN
+    OPEN SYMMETRIC KEY clave_simetrica_emp 
+    DECRYPTION BY PASSWORD = 'clavesegura1';
+
     BEGIN TRANSACTION;
     BEGIN TRY
-		
-        --INSERT INTO infraestructura.Empleado (Legajo, Nombre, Apellido, DNI, Direccion, EmailPersonal, EmailEmpresa, CUIL, Turno, Cargo, Sucursal)
-		--VALUES (@Legajo, @Nombre, @Apellido, @DNI, @Direccion, @EmailPersonal, @EmailEmpresa, @CUIL, @Turno, @Cargo, @Sucursal);
-
-		-----------------CAMBIO--------------------------
-		INSERT INTO infraestructura.empleado (Legajo, Nombre, Apellido, DNI, DireccionCifrada, EmailPersonalCifrado, EmailEmpresa, CUIL, Turno, Cargo, Sucursal)
-		VALUES 
-            (
-                @Legajo,
-                @Nombre,
-                @Apellido,
-                @DNI,
-                -- Encriptaci?n de Direccion y EmailPersonal al momento de la inserci?n
-                EncryptByPassPhrase(@FraseClave, @Direccion, 1, CONVERT(VARBINARY, @Legajo)),
-                EncryptByPassPhrase(@FraseClave, @EmailPersonal, 1, CONVERT(VARBINARY, @Legajo)),
-                @EmailEmpresa,
-                @CUIL,
-                @Turno,
-                @Cargo,
-                @Sucursal
-            );
-        ----------------------------------------------------------
-
+        UPDATE infraestructura.empleado
+        SET 
+            DNI = EncryptByKey(Key_GUID('clave_simetrica_emp'), DNI),
+            Direccion = EncryptByKey(Key_GUID('clave_simetrica_emp'), Direccion),
+            CUIL = EncryptByKey(Key_GUID('clave_simetrica_emp'), CUIL),
+            Encriptado = 1
+        WHERE Encriptado = 0;
 
         COMMIT TRANSACTION;
-        PRINT 'Empleado insertado correctamente.';
     END TRY
     BEGIN CATCH
         ROLLBACK TRANSACTION;
-        PRINT 'Error al intentar insertar el Empleado: ' + ERROR_MESSAGE();
+        PRINT 'Error al encriptar los datos: ' + ERROR_MESSAGE();
     END CATCH;
+    CLOSE SYMMETRIC KEY clave_simetrica_emp;
 END;
 GO
 
 
+---------------------------------- ENCRIPTAMOS --------------------------------------
+
+EXEC EncriptarEmpleados;
+GO
+
+-- Vemos los datos despues de encriptar
+SELECT 
+    Legajo,
+    DNI,
+    Direccion,
+    CUIL,
+    Encriptado
+FROM infraestructura.Empleado
 
 
-*/
+
+------------------------------ DESENCRIPTAMOS ----------------------------------------
+
+OPEN SYMMETRIC KEY clave_simetrica_emp 
+DECRYPTION BY PASSWORD = 'clavesegura1';
+
+SELECT 
+    Legajo,
+    CONVERT(NVARCHAR(256), DecryptByKey(DNI)) AS DNI_Original,
+    CONVERT(NVARCHAR(256), DecryptByKey(Direccion)) AS Direccion_Original,
+    CONVERT(NVARCHAR(256), DecryptByKey(CUIL)) AS CUIL_Original,
+    Encriptado
+FROM infraestructura.Empleado
+CLOSE SYMMETRIC KEY clave_simetrica_emp;
+
